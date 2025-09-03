48bd48280902fda970eff99ac8d21819cf3e1e28
Daniel Rivas <danielrivasmd@gmail.com>
2025-09-03 13:45:16 +0200

    update roadmap

--- cmd/cmdInvoke.go
+++ cmd/cmdInvoke.go
@@ -72,6 +72,7 @@ func init() {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// TODO: refactor as non-error pre-run
 func preInvoke(cmd *cobra.Command, args []string) error {
 	const op = "lilith.invoke.pre"
 	configName = args[0]
