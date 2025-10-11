27b5987a10136e8aef3a3d20999853cf873a22fd
Daniel Rivas <danielrivasmd@gmail.com>
2025-10-11 18:10:57 +0300

    patch zellij interpret

--- interpret/zellij.sh
+++ interpret/zellij.sh
@@ -8,11 +8,11 @@ source "${HOME}/.lilith/.workflow_config.sh"
 ####################################################################################################
 
 # interpret
-babel embed --program zellij-entersearch --target "${zmodes}/entersearch.kdl"
-babel embed --program zellij-locked      --target "${zmodes}/locked.kdl"
-babel embed --program zellij-renamepane  --target "${zmodes}/renamepane.kdl"
-babel embed --program zellij-renametab   --target "${zmodes}/renametab.kdl"
-babel embed --program zellij-search      --target "${zmodes}/search.kdl"
-babel embed --program zellij-shared      --target "${zmodes}/shared.kdl"
+babel embed --program zellij-enter  --target "${zmodes}/entersearch.kdl"
+babel embed --program zellij-lock   --target "${zmodes}/locked.kdl"
+babel embed --program zellij-rpane  --target "${zmodes}/renamepane.kdl"
+babel embed --program zellij-rtab   --target "${zmodes}/renametab.kdl"
+babel embed --program zellij-search --target "${zmodes}/search.kdl"
+babel embed --program zellij-shared --target "${zmodes}/shared.kdl"
 
 ####################################################################################################
