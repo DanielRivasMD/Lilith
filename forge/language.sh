cf4cb95c7bd4e21185a110ac3dba6bb90ab05eec
Daniel Rivas <danielrivasmd@gmail.com>
2026-04-19 14:56:31 +0300

    update language forge

--- forge/language.sh
+++ forge/language.sh
@@ -11,6 +11,7 @@ source "${HOME}/.lilith/.workflow_config.sh"
 mbombo --in "${hlangs}" --out "${helix}/languages.toml" \
   --files "awk.toml" \
   --files "bash.toml" \
+  --files "csv.toml" \
   --files "docker.toml" \
   --files "clojure.toml" \
   --files "git.toml" \
@@ -18,12 +19,17 @@ mbombo --in "${hlangs}" --out "${helix}/languages.toml" \
   --files "html.toml" \
   --files "json.toml" \
   --files "julia.toml" \
+  --files "kbd.toml" \
+  --files "kdl.toml" \
+  --files "lua.toml" \
   --files "markdown.toml" \
+  --files "nu.toml" \
   --files "python.toml" \
   --files "R.toml" \
   --files "rust.toml" \
   --files "scala.toml" \
   --files "sql.toml" \
+  --files "ssh-config.toml" \
   --files "toml.toml" \
   --files "yaml.toml"
 
